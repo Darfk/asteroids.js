cd7cea82be7ad7291f0bea9c09130557de938d82
ejopony <ejopony@gmail.com>
2013-09-22 16:18:24 +1000

    display screen

---
+++ js/asteroids.js
@@ -0,0 +1,9 @@
+/* asteroids.js
+   A simple asteroids game
+*/
+
+var screen=document.getElementById("screen");
+var ctx=screen.getContext("2d");
+
+ctx.fillStyle="#000";
+ctx.fillRect(0, 0, screen.width, screen.height);
