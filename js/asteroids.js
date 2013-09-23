7b0ef9aa76e3027103557b82309c86c069ffb85e
ejopony <ejopony@gmail.com>
2013-09-23 12:06:58 +1000

    Keyboard input, fancier ship image

--- js/asteroids.js
+++ js/asteroids.js
@@ -5,6 +5,15 @@ var screen = document.getElementById("screen");
 var ctx = screen.getContext("2d");
 var objects = [];
 
+// Config
+
+// ship thrust in pixels/second/second
+thrust = 100;
+// Topspeed of ship in pixels/second
+topSpeed = 500;
+// Ship speed lost per second
+speedDecay = 0.2;
+
 ctx.fillStyle = "#000";
 ctx.strokeStyle = "#ffffff";
 
@@ -17,9 +26,9 @@ function GameObject() {
     // Velocity in pixels/second
     this.vx = 0;
     this.vy = 0;
-
+    
     // Radius of object in pixels
-    this.r = 50;
+    this.r = 20;
 
     objects.push(this);
 }
@@ -76,6 +85,98 @@ function run(time) {
         draw();
 }
 
-var testObject = new GameObject();
+var ship = new GameObject();
+// Ship-specific state
+ship.thrustingUp = false;
+ship.thrustingDown = false;
+ship.thrustingLeft = false;
+ship.thrustingRight = false;
 
+// Ship-specific state update
+ship.update = function(dt) {
+    // Apply acceleration
+    if (this.thrustingUp)
+        this.vy -= thrust * dt;
+    if (this.thrustingDown)
+        this.vy += thrust * dt;
+
+    if (this.thrustingLeft)
+        this.vx -= thrust * dt;
+    if (this.thrustingRight)
+        this.vx += thrust * dt;
+    
+    // Velocity decay
+    this.vx -= (this.vx * speedDecay * dt);
+    this.vy -= (this.vy * speedDecay * dt);
+    
+    // Clamp top speed
+    speed = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+    if (speed / topSpeed > 1) {
+        this.vx /= speed / topSpeed;
+        this.vy /= speed / topSpeed;
+    }
+
+    GameObject.prototype.update.call(this, dt);
+};
+
+// Ship-specific drawing
+ship.draw = function() {
+    ctx.beginPath();
+    ctx.save();
+    var pi = Math.PI;
+
+    ctx.translate(this.x, this.y);
+    ctx.moveTo(0, -this.r);
+    ctx.lineTo(Math.cos(pi/2 + 2*pi/3)*this.r, -Math.sin(pi/2 + 2*pi/3)*this.r);
+    ctx.lineTo(0, 0);
+    ctx.lineTo(Math.cos(pi/2 + 4*pi/3)*this.r, -Math.sin(pi/2 + 4*pi/3)*this.r);
+    ctx.closePath();
+
+    ctx.stroke()
+    ctx.restore();
+
+    GameObject.prototype.draw.call(this);
+}
+
+// Keyboard input
+function handleKeydown(event) {
+    // w, up
+    if (event.keyCode == 38 || event.keyCode == 87)
+        ship.thrustingUp = true;
+
+    // d, right
+    if (event.keyCode == 39 || event.keyCode == 68)
+        ship.thrustingRight = true;
+
+    // s, down
+    if (event.keyCode == 40 || event.keyCode == 83)
+        ship.thrustingDown = true;
+
+    // a, left
+    if (event.keyCode == 37 || event.keyCode == 65)
+        ship.thrustingLeft = true;
+}
+
+function handleKeyup(event) {
+    // w, up
+    if (event.keyCode == 38 || event.keyCode == 87)
+        ship.thrustingUp = false;
+
+    // d, right
+    if (event.keyCode == 39 || event.keyCode == 68)
+        ship.thrustingRight = false;
+
+    // s, down
+    if (event.keyCode == 40 || event.keyCode == 83)
+        ship.thrustingDown = false;
+
+    // a, left
+    if (event.keyCode == 37 || event.keyCode == 65)
+        ship.thrustingLeft = false;
+}
+
+document.addEventListener('keydown', handleKeydown);
+document.addEventListener('keyup', handleKeyup);
+
+document.add
 run();
